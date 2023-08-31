31dee616281f58f6cb07d90f7459ca5ad26f59e5
mainakcs <mainakbiswas64@gmail.com>
2023-08-31 21:54:38 +0530

    add the project files

---
+++ dataCollection.py
@@ -0,0 +1,61 @@
+# YouTube Video link - https://www.youtube.com/watch?v=wa2ARoUUdU8
+
+from chardet import detect
+import cv2 as cv
+from cvzone.HandTrackingModule import HandDetector
+from matplotlib import image
+import numpy as np
+import math
+import time
+
+cap = cv.VideoCapture(0)
+detector = HandDetector(maxHands=2)
+
+offset = 20
+imgSize = 300
+
+folder = "Data/B"
+counter = 0
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x,y,w,h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+        imgCrop = img[y-offset : y+h+offset , x-offset : x+w+offset]
+
+        imgCropShape = imgCrop.shape
+
+
+
+        aspectRatio = h/w
+
+        if aspectRatio >1:
+            k = imgSize/h
+            wCal = math.ceil(k*w)
+            imgResize = cv.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape    
+            wGap = math.ceil((imgSize-wCal)/2)
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+            
+        else:
+            k = imgSize/w
+            hCal = math.ceil(k*h)
+            imgResize = cv.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape    
+            hGap = math.ceil((imgSize-hCal)/2)  
+            imgWhite[hGap:hCal + hGap, :] = imgResize        
+
+        cv.imshow("ImageCrop", imgCrop)
+        cv.imshow("ImageWhite", imgWhite)
+
+
+    cv.imshow("Image",img)
+    key = cv.waitKey(1)
+    if key == ord("s"):
+        counter += 1; 
+        cv.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        print(counter)
